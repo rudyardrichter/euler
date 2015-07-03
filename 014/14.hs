5b94c10b0cbc8a6e334cdae16c606f639e6dde37
dQuixote <rudyardrichter@uchicago.edu>
2015-07-03 11:14:58 +0200

    problem 14, faster using Array

--- 014/14.hs
+++ 014/14.hs
@@ -1,17 +1,28 @@
-import Data.List (foldl1')
+import Data.Array
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 
-collatzLen :: Int -> Int
-collatzLen = loop
+step :: Int -> Int
+step n
+    | r == 0    = q
+    | otherwise = succ $ 3 * n
   where
-    loop 1 = 0
-    loop n = succ . loop . step $ n
-    step n
-        | r == 0    = q
-        | otherwise = succ $ 3 * n
+    (q, r) = quotRem n 2
+
+collatzLengthsTo :: Int -> Array Int Int
+collatzLengthsTo bound = ar
+  where
+    ar = listArray (1, bound) $ 0 : [collatz x | x <- [2..bound]]
+    collatz x
+        | x' <= bound   = succ $ ar ! x'
+        | otherwise     = succ $ collatz x'
       where
-        (q, r) = quotRem n 2
+        x' = step x
 
 main :: IO ()
 main = print
-     . foldl1' (\acc x -> if collatzLen x >= collatzLen acc then x else acc)
-     $ [1..999999]
+     . fst
+     . maximumBy (comparing snd)
+     . assocs
+     . collatzLengthsTo
+     $ 1000000
