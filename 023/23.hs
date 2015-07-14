4f49e3a5fe219340efc1c6d400aa5082e5775ae3
dQuixote <rudyardrichter@uchicago.edu>
2015-07-14 07:13:47 -0500

    problem 23

---
+++ 023/23.hs
@@ -0,0 +1,21 @@
+import Data.Array.Unboxed
+import Primes (divisors)
+
+sumOfAbundantsTo :: Int -> Int -> Bool
+sumOfAbundantsTo bound n = any (abundants !)
+                         . map (n -)
+                         . takeWhile (<= n `div` 2)
+                         . filter (abundants !)
+                         $ [1..bound]
+  where
+    abundants :: UArray Int Bool
+    abundants = listArray (1, bound) $ map isAbundant [1..bound]
+    isAbundant n = sum (divisors n) > n
+
+main :: IO ()
+main = print
+     . sum
+     . filter (not . sumOfAbundantsTo bound)
+     $ [1..bound]
+  where
+    bound = 28123
