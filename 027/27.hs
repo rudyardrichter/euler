7e2e5a456ba16df2799976bcd69f775eb7bd2446
dQuixote <rudyardrichter@uchicago.edu>
2015-07-17 07:04:04 -0500

    problem 27

---
+++ 027/27.hs
@@ -0,0 +1,21 @@
+import Control.Applicative ((<$>), (<*>))
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Primes (primesTo)
+
+main :: IO ()
+main = print
+     . result
+     . snd
+     . maximumBy (comparing fst)
+     . map (tuple $ length . takeWhile (flip Set.member primes) . evalQF)
+     $ (,) <$> [-1000..1000] <*> [-1000..1000]
+  where
+    result (b, c) = b * c
+    tuple f x = (f x, x)
+    primes = Set.fromDistinctAscList $ primesTo 1000
+    evalQF (b, c) = map (\x -> abs $ x * x + b * x + c) [1..]
