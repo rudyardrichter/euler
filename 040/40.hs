9f472690ae842ca79869a35a961c0a233be35cbb
dQuixote <rudyardrichter@uchicago.edu>
2015-08-04 11:53:00 -0500

    points for style (haha)

--- 040/40.hs
+++ 040/40.hs
@@ -4,4 +4,5 @@ main :: IO ()
 main = print . product . map (digitToInt . d) . take 7 . iterate (* 10) $ 1
   where
     d n = (!! (n - 1)) naturals
+    -- d = (!!) naturals . pred
     naturals = concatMap show [1..]
