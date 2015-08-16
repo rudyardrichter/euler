89b0f950493e5ca6d0a1639395b97c1d7a6bf5dc
dQuixote <rudyardrichter@uchicago.edu>
2015-08-16 09:19:29 -0500

    more concise 46

--- 046/46.hs
+++ 046/46.hs
@@ -1,16 +1,10 @@
-import Euler ((&.&))
+import Euler ((|.|), first)
 import Primes (isPrime)
 
 valid :: Int -> Bool
-valid n = any isPrime
-        . map (n -)
-        . takeWhile (< n)
-        $ twiceSquares
+valid n = any isPrime . map (n -) . takeWhile (< n) $ twiceSquares
   where
     twiceSquares = map (\i -> 2 * i * i) [1..]
 
 main :: IO ()
-main = print
-     . head
-     . filter ((not . valid) &.& (not . isPrime))
-     $ [9, 11..]
+main = print $ first (not . (valid |.| isPrime)) [9, 11..]
