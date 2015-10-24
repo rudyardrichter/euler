01f0c21d11faa852dc8f07b7b387275acd90c897
dQuixote <rudyardrichter@uchicago.edu>
2015-10-24 16:11:10 -0500

    28 with arrows

--- 028/28.hs
+++ 028/28.hs
@@ -1,5 +1,13 @@
+import Control.Arrow
+
 ring :: Int -> Int
 ring n = 4 * (2 * n + 1) ^ 2 - 12 * n
 
+ringA :: Int -> Int
+ringA = ((* 4) . (^ 2) . succ . (* 2)) &&& (* 12) >>> arr (uncurry (-))
+
 main :: IO ()
-main = print . succ . sum $ map ring [1..500]
+main = print . succ . sum . map ring $ [1..500]
+
+mainA :: IO ()
+mainA = print . succ . sum . map ringA $ [1..500]
