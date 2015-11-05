92bf4cdb64c62d318cc7af6898ac4956cebf4f49
dQuixote <rudyardrichter@uchicago.edu>
2015-11-04 20:17:25 -0600

    added arrows solution to 30

--- 030/30.hs
+++ 030/30.hs
@@ -1,3 +1,4 @@
+import Control.Arrow
 import Data.Char (digitToInt)
 
 import Euler (digits)
@@ -7,3 +8,9 @@ main = print . sum . filter f $ [10..200000]
   where
     f x = fifthPowerSum x == x
     fifthPowerSum = sum . map (^ 5) . digits
+
+mainA :: IO ()
+mainA = print
+      . sum
+      . filter (sum . map (^ 5) . digits &&& id >>> arr (uncurry (==)))
+      $ [10..200000]
