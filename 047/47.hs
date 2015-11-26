bbc2e81416edfd0c3483ea9cdfae1606031393a5
dQuixote <rudyardrichter@uchicago.edu>
2015-11-26 15:18:38 -0600

    small change to problem 47

--- 047/47.hs
+++ 047/47.hs
@@ -3,11 +3,11 @@ import Data.List (nub)
 import Euler (first)
 import Primes (primePowers)
 
-distinctFactors :: Int -> Bool
-distinctFactors n = length factors == 16
+distinctFactors :: Int -> Int -> Bool
+distinctFactors k n = k == length factors
   where
     factors = nub $ concatMap (map combine . primePowers) [n..n + 3]
     combine (a, b) = a * b
 
 main :: IO ()
-main = print $ first distinctFactors [1..]
+main = print $ first (distinctFactors 16) [1..]
